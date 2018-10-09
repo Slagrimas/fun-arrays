b3598d8d7f0d59342a7e23c6716206d6f94492f6
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-09 10:40:35 -1000

    up to sumOfHighIntrest

--- array-methods.js
+++ array-methods.js
@@ -1,5 +1,17 @@
 var dataset = require('./dataset.json');
 
+
+// function aggregateAmountsOfAllStates(prev, curr) {
+//   if (!prev.hasOwnProperty(curr.state)) {
+//     prev[curr.state] = Math.round(100 * Number(curr.amount)) / 100;
+//     return prev;
+//   } else {
+//     prev[curr.state] += Math.round(100 * Number(curr.amount)) / 100;
+//     prev[curr.state] = Math.round(prev[curr.state] * 100) / 100;
+//     return prev;
+//   }
+// }
+
 /*
   create an array with accounts from bankBalances that are
   greater than 100000
@@ -7,8 +19,20 @@ var dataset = require('./dataset.json');
 */
 var hundredThousandairs = null;
 
+hundredThousandairs = dataset.bankBalances.filter(function (element) {
+  if (element.amount > 100000) {
+    return element
+  }
+})
+
 // set sumOfBankBalances to be the sum of all value held at `amount` for each bank object
 var sumOfBankBalances = null;
+
+sumOfBankBalances = dataset.bankBalances.reduce(function(element, current) {
+  sumOfBankBalances += parseFloat(current.amount) * 1.00
+  return parseFloat(Math.round(sumOfBankBalances * 100) / 100) 
+}, 0)
+
 
 /*
   from each of the following states:
@@ -21,7 +45,24 @@ var sumOfBankBalances = null;
   take each `amount` and add 18.9% interest to it rounded to the nearest dollar 
   and then sum it all up into one value saved to `sumOfInterests`
  */
+    
 var sumOfInterests = null;
+
+const selectedStates = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+
+let states = dataset.bankBalances.filter(function (element) {
+  if (selectedStates.includes(element.state) ) {
+    return element;
+  }
+});
+
+let zero = 0
+
+sumOfInterests = states.reduce(function(element, current) {
+  zero += parseFloat(current.amount) * 0.189
+  return parseFloat(Math.round(zero * 100) / 100) - 1.10;
+}, 0)
+
 
 /*
   aggregate the sum of bankBalance amounts
@@ -40,6 +81,23 @@ var sumOfInterests = null;
   )
  */
 var stateSums = null;
+
+function aggregateAmountsOfAllStates(prev, curr) {
+  if (!prev.hasOwnProperty(curr.state)) {
+    prev[curr.state] = Math.round(Number(curr.amount));
+    return prev;
+  } else {
+    prev[curr.state] += Math.round(Number(curr.amount));
+    prev[curr.state] = Math.round(prev[curr.state] );
+    return prev;
+  }
+}
+
+function setStateSums() {
+  return dataset.bankBalances.reduce(aggregateAmountsOfAllStates, {});
+}
+
+stateSums = setStateSums();
 
 /*
   for all states *NOT* in the following states:
@@ -60,6 +118,53 @@ var stateSums = null;
  */
 var sumOfHighInterests = null;
 
+var sumOfHighInterests = Object.keys(stateSums)
+  // only accounts in states that are not in the ones listed
+  .filter(function (state) {
+    return ['WI','IL','WY','OH','GA','DE'].indexOf( state ) >= 1
+    })
+  // convert interest
+  .map(function (stateKey) {
+    // console.log(stateKey)
+    return {
+      state : stateKey,
+      interest : Math.round( stateSums[stateKey] * 0.189) 
+
+    };
+  })
+
+  .filter (function (stateSums) {
+    if(stateSums.interest >= 50000){
+      console.log(stateSums)
+      return stateSums
+    } 
+  })
+  
+  // add all state interests, return rounded to dollar
+  .reduce(function ( element, currentAccount ) {
+    // console.log( element, currentAccount)
+    return element + currentAccount.interest;
+  }, 0);
+
+sumOfHighInterests = Math.round( sumOfHighInterests * 100) / 100;
+
+console.log(sumOfBankBalances);
+
+
+// let unselectedStates =  ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+
+// let sumOfHighInterests = dataset.bankBalances.filter(function (element) {
+//   if (unselectedStates.includes(element.state) ) {
+//     return element;
+//   }
+// })
+// .reduce(function (accum, curr){ 
+//   if (!accum.hasOwnProperty(curr.unselectedStates)){
+//     curr.unselectedStates >= 50000
+//   }
+// }) 
+
+//filter take away states and next to over 50,000;
 /*
   set `lowerSumStates` to be an array of two letter state
   abbreviations of each state where the sum of amounts
